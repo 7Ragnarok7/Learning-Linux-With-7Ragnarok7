b6f944cf83e2804956e91fe40f9a4ffc5d14481b
7Ragnarok7 <anorak318@gmail.com>
2020-07-25 16:13:13 +0530

    Rearranged some files

--- day5/add.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -x
-x=10
-y=20
-z=$(( $x + $y ))
-echo $z
--- day5/user_input.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -x
-read -p "Enter the first number: " x
-read -p "Enter the second number: " y
-z=$(( $x + $y ))
-echo $z
